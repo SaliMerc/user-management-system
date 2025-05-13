1ef50f67b264fe00e8e688fbedeb90551e7e42cb
SaliMerc <nyawadesaline@gmail.com>
2025-05-13 14:44:29 +0300

    modified the api urls for identification

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,11 +4,11 @@ from .import views
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
-    path('get-users/', views.get_users, name='get-users'),
+    path('api/get-users/', views.get_users, name='get-users'),
 
-    path('create-user/', views.create_user, name='create-user'),
+    path('api/create-user/', views.create_user, name='create-user'),
 
-    path('user/update-user-details/<int:id>', views.update_user_details, name='update-user'),
-    path('user/change-password/<int:id>', views.change_password, name='change-password'),
-    path('user/delete/<int:id>', views.delete_user, name='delete-user'),
+    path('api/user/update-user-details/<int:id>', views.update_user_details, name='update-user'),
+    path('api/user/change-password/<int:id>', views.change_password, name='change-password'),
+    path('api/user/delete/<int:id>', views.delete_user, name='delete-user'),
 ]
